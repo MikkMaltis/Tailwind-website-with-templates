e268cef5edc6cbb442ad467980cdf51d79525634
MikkMaltis <maltismikk@gmail.com>
2023-10-26 21:14:18 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font-family': 'Fondamento, cursive',
+        'font-family': 'Gabriela, serif',
+        'font-family': 'Graduate, serif',
+        'font-family': 'Inter, sans-serif',
+        'font-family': 'Lato, sans-serif',
+        'font-family': 'Nunito Sans, sans-serif',
+        'font-family': 'Roboto, sans-serif',
+        'font-family': 'Vina Sans, sans-serif',
+        'font-family': 'Ysabeau, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
